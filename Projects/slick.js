c98860846a0a28ef9ce0670df8548eadd7a6fce4
speeeedy-899 <utsav.apurva@gmail.com>
2021-07-09 19:19:53 +0530

    Added scroll animation and minor tweaks

--- Projects/slick.js
+++ Projects/slick.js
@@ -1,3 +1,8 @@
+$('.carousel').on('init', function () {
+  $('.carousel').css({visibility: 'visible'});
+});
+
+
 $('.carousel').slick({
   adaptiveHeight: true,
   arrows: true,
@@ -6,6 +11,7 @@ $('.carousel').slick({
   centerPadding: "70px",
   dots: true,
   initialSlide: 0,
+  lazyLoad: "ondemand",
   nextArrow: "<img class='slick-next' src='../right.svg'>",
   prevArrow: "<img class='slick-prev' src='../left.svg'>",
   pauseOnFocus: false,
