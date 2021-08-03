76766da0c045ee71600611b5981636ed087632f9
speeeedy-899 <utsav.apurva@gmail.com>
2021-08-03 20:46:55 +0530

    Add content to media

---
+++ Media/slick.js
@@ -0,0 +1,23 @@
+$('.carousel').on('init', function () {
+  $('.carousel').css({visibility: 'visible'});
+});
+
+
+$('.carousel').slick({
+  adaptiveHeight: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3300,
+  centerMode: true,
+  centerPadding: "0",
+  cssEase: "cubic-bezier(.87,0,.13,1)",
+  // dots: true,
+  initialSlide: 0,
+  // lazyLoad: "ondemand",
+  pauseOnFocus: false,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+  slidesToShow: 1,
+  // variableWidth: true,
+  speed: 700,
+});
